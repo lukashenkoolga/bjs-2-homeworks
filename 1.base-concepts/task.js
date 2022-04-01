5815367d74cfb85c66c363cbe28e7e0ff806b477
Olga Lukashenko <lukashenko.93@bk.ru>
2022-04-01 13:35:38 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,12 @@
+'use strict';
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  let D = (Math.pow(b,2)) - 4 * a * c;
+  let x = [] ;
+  if (D == 0) {
+     x.push(-b / (2 * a));
+  } else if (D > 0) {
+     x.push(((-b + (Math.sqrt(D))) / (2 * a)));
+     x.push(((-b - (Math.sqrt(D))) / (2 * a)));
+  }
+  return x;
 }
