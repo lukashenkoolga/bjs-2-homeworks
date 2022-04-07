537d06a23d5b8a42f765aed780afb695f9320d2c
Olga Lukashenko <lukashenko.93@bk.ru>
2022-04-07 19:13:52 +0300

    Initial

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,48 @@
-// Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  let sum = 0;
+  let avg = 0;
+  let min =Infinity;
+  let max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < min) {
+          min = arr[i];
+      }
+      if (arr[i] > max) {
+          max = arr[i];
+      }
+      sum += arr[i];
+  }
+      avg = (sum / arr.length).toFixed(2);
+      avg = Number(avg);
+  return {
+      min: min,
+      max: max,
+      avg: avg
+  };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr [i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = func(arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++){
+    let result = func(arrOfArr[i]);
+    if (result > max) {
+      max = result;
+    }
+  }  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let arrParams = getArrayParams(arr);
+  return Math.abs(arrParams.max - arrParams.min);
 }
