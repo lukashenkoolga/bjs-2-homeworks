23849040b39a82f2140fe3bb4d3b6a865aa1fef9
Olga Lukashenko <lukashenko.93@bk.ru>
2022-04-09 18:28:11 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return (arr1.length === arr2.length) && arr1.every((val, idx)=> val === arr2[idx]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return  arr.filter(val => val > 0).filter((val) =>val %3 === 0).map((val)=> val *10);
 }
