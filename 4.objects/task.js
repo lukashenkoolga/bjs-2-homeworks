abd0f3a9f508957d79a792d29f8f0f6cd9670fa4
Olga Lukashenko <lukashenko.93@bk.ru>
2022-04-14 20:31:23 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name=name;
+    this.gender=gender;
+    this.age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject=subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks===undefined){
+    this.marks=[mark];
+  }
+  else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(...mark) {
+  if(this.marks===undefined){
+    this.marks=[...mark];
+  } else {
+    this.marks.push(...mark);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  let sum = 0;
+  this.marks.forEach((mark) => {sum += parseInt(mark)});
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete subject;
+  delete this.marks;
+  this.excluded = reason;
+}
